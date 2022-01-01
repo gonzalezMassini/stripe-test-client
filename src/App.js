e29a24fbaf4b07a53b089e91b2801eccc783878d
gonzalezmassini <gonzalez.massini@gmail.com>
2021-12-31 22:36:13 -0400

    .

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
     fetch("https://stripe-testing-server.herokuapp.com/create-checkout-session", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",'Access-Control-Allow-Origin':'https://stripe-test-client.herokuapp.com/',
+      "Content-Type": "application/json",'Access-Control-Allow-Origin':'*',
     },
     body: JSON.stringify({
       items: [
